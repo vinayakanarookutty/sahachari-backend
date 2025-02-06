0f2733e7e18faf2fdcf2809fcba5bf9c7ec538d1
Vinayak S <vinayaksukhalal@gmail.com>
2025-02-06 14:31:15 +0530

    Resolved the bug issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "express-local",
-        script: "server.js",
+        script: "node index.js",
         env: {
           MONGO_URI: "mongodb+srv://Dilshad:dilshad1041@cluster0.mcgvrw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
           PORT: 5000
@@ -10,7 +10,7 @@ module.exports = {
       },
       {
         name: "express-cloud",
-        script: "server.js",
+        script: "node index.js",
         env: {
           MONGO_URI: "mongodb://admin:securepassword@54.173.132.195:27017/admin",
           PORT: 3000
